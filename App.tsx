66d0b7479449531997c5097d32b3a06d55b8a302
Fernando Modafaris de Araujo <74414642+fmodafaris@users.noreply.github.com>
2020-12-15 21:51:57 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Keyboard,
+} from 'react-native';
+import {format} from 'date-fns';
+
+interface ListaTarefasInterface {
+  titulo: string;
+  data: Date;
+}
+
+const App = () => {
+  const [novaTarefa, setNovaTarefa] = useState('');
+  const [listaTarefas, setListaTarefas] = useState <Array<ListaTarefasInterface>>([]);
+  const gravaTarefa = () => {
+    if (novaTarefa === ''){
+      return false;
+    }
+    Keyboard.dismiss();
+    setNovaTarefa('');
+    const novaLista = [...listaTarefas];
+
+    novaLista.unshift({
+      titulo: novaTarefa,
+      data: new Date(),
+    });
+    setListaTarefas(novaLista);
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <View style={{flex: 1, padding: 16}}>
+        <Image
+          source={require('./logo.png')}
+          style={{height: 58, width: '75%'}}
+          resizeMode="contain"
+        />
+        <View style={{flexDirection: 'row'}}>
+          <TextInput
+            value={novaTarefa}
+            onChangeText={(valor) => {
+              setNovaTarefa(valor);
+            }}
+            placeholder="Adicionar Tarefa"
+            style={{
+              flex: 1,
+              marginRight: 12,
+              borderBottomWidth: 1,
+              borderBottomColor: '#707070',
+            }}
+          />
+
+          <TouchableOpacity
+            onPress={() => {
+              gravaTarefa();
+            }}
+            style={{
+              backgroundColor: '#FFAA00',
+              width: 52,
+              borderRadius: 26,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{color: '#fff', fontSize: 26}}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{margintop: 24}}>
+          {listaTarefas.map((item, index) => (
+            <View
+              key={'item' + index}
+              style={{
+                backgroundColor: '#f5f8f9',
+                padding: 8,
+                borderLeftWidth: 4,
+                borderLeftColor: '#1abc9c',
+                marginBottom: 8,
+              }}>
+              <Text
+                style={{
+                  textAlign: 'right',
+                  fontSize: 12,
+                  fontWeight: 'bold',
+                }}>
+                {format(item.data, 'dd/MM/yyyy HH:mm')}
+              </Text>
+              <Text style={{fontSize: 16}}>{item.titulo}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default App;
